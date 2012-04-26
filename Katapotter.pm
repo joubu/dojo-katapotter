9cfd685d603c693f222b91867559f212e8e13790
Jonathan Druart <druartjonathan@gmail.com>
2012-04-26 20:07:30 +0200

    All tests successful

--- Katapotter.pm
+++ Katapotter.pm
@@ -1,27 +1,52 @@
 package Katapotter;
 
 use Modern::Perl;
-use List::MoreUtils qw/uniq/;
+use List::MoreUtils qw/uniq first_index/;
+use Data::Dumper;
 
 sub price {
     my @booklist = @_;
-    my $nb_books = scalar(@booklist);
+    return 0 if @booklist < 1;
     my $unit_price = 8;
+    return $unit_price if @booklist == 1;
     my $remise = {
         0 => 0,
         1 => 1,
         2 => 0.95,
-        3 => 0.90
+        3 => 0.9,
+        4 => 0.8,
+        5 => 0.75,
     };
-    my $price;
-    my $unique_books=scalar(uniq(@booklist)) ;
-    if($nb_books == 2 and $booklist[0] == $booklist[1]) {
-	$price = 2*$unit_price;
-    } elsif (($nb_books == 3 ) and ($unique_books == 2)) {
-          $price = $unique_books*$unit_price*$remise->{$unique_books}+$unit_price;
-    } else {
-	$price = $nb_books*$unit_price*$remise->{$nb_books};
-    }
-    return $price;
+    my @unique_books = uniq @booklist;
+    @booklist = array_diff( \@booklist, \@unique_books );
+    my $nb_unique_books = @unique_books;
+    return $nb_unique_books * $unit_price * $remise->{$nb_unique_books} + price( @booklist );
 }
+
+sub array_diff {
+    my ( $a, $b ) = @_;
+    my @already_replaced;
+    return grep {
+        my $t = $_;
+        my $r2 = grep {$_ == $t} @already_replaced;
+        push @already_replaced, $t;
+        $r2;
+    } @$a;
+}
+
+sub array_diff2 {
+    my ( $a, $b ) = @_;
+    my @already_replaced;
+    my @c;
+    for my $f( @$a ) {
+        if ( grep /$f/, @$b ) {
+            my $index = first_index { defined $_ and ($_ eq $f) } @$a;
+            delete @$a[$index] if not grep /$f/, @already_replaced;
+            push @already_replaced, $f;
+        }
+    }
+    @c = map {( not defined $_ ) ? () : $_} @$a;
+    return @c;
+}
+
 1;
--- katapotter.t
+++ katapotter.t
@@ -3,11 +3,15 @@ use Modern::Perl;
 use Test::More;
 use Katapotter;
 
-my $nb_test = 5;
-plan tests => $nb_test;
-
 is(Katapotter::price(1), 8, "1 book = 8€");
-is(Katapotter::price(1,2), 8*2*0.95, "2 books differents");
-is(Katapotter::price(1,2,3), 8*3*0.90, "3 books differents");
+is(Katapotter::price(1,2), 8*2*0.95, "2 different books");
+is(Katapotter::price(1,2,3), 8*3*0.90, "3 different books");
 is(Katapotter::price(1,1), 8*2, "2 identical books");
 is(Katapotter::price(1,1,2), 8*2*0.95+8, "2 identical books and one book");
+is(Katapotter::price(1,2,3,1), 8*3*0.9+8, "2 identical books and two books");
+is(Katapotter::price(1,2,3,1,1), 8*3*0.9+8*2, "3 identical books and two books in order");
+is(Katapotter::price(1,2,1,3,1), 8*3*0.9+8*2, "3 identical books and two books not in order");
+is(Katapotter::price(1,2,1,3,1,1), 8*3*0.9+8*3, "4 identical books and two books");
+is(Katapotter::price(1,2,3,4,5,1,5), 8*5*0.75+8*2*0.95, "5 different books and 2 books");
+
+done_testing;
